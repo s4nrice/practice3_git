a898bf3812a2f1a98f89e003fbf4060dc2cd3551
zyryanov <zantond31@gmail.com>
2025-04-22 17:52:59 +0300

    #3 Fixed edge case when max_value=min_value

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def normalize_to_minus_one_one(numbers: list[float]) -> list[float]:
     max_val = max(numbers)
     min_val = min(numbers)
 
+    if max_val == min_val:
+        return [0] * len(numbers)
+
     result = []
     for number in numbers:
         norm = (2 * (number - min_val) / (max_val - min_val)) - 1
